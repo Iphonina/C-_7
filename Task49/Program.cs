300b490153e05adc52ca6b20dddee1bfff14bce4
Yana <leduschenkova6@gmail.com>
2022-10-07 09:06:50 +0300

    Add Task 49

---
+++ Task49/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Новый массив будет выглядеть 
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int J = 0; J < matr.GetLength(1); J++)
+        {
+            matr[i, J] = new Random().Next(-10, 10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int J = 0; J < matr.GetLength(1); J++)
+        {
+            Console.Write($"{matr[i, J]} ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+void NewMatrix(int[,] table)
+{
+    for (int i = 0; i < matrix.GetLength(0); i+= 2)
+    {
+        for (int J = 0; J < matrix.GetLength(1); J+= 2)
+        {
+            matrix[i, J] = matrix[i, J] * matrix[i, J];
+        }
+    }
+}
+Console.WriteLine();
+NewMatrix(matrix);
+PrintArray(matrix);

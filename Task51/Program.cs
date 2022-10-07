6a78be57cdfa7c1725c70b4fbc5f1c669b0b5ad8
Yana <leduschenkova6@gmail.com>
2022-10-07 09:07:23 +0300

    Add Task 51

---
+++ Task51/Program.cs
@@ -0,0 +1,56 @@
+﻿// **Задача 51:** Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int J = 0; J < matr.GetLength(1); J++)
+        {
+            matr[i, J] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int J = 0; J < matr.GetLength(1); J++)
+        {
+            Console.Write($"{matr[i, J]} ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+int sum = 0;
+
+void SumDiagonal(int[,] table)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int J = 0; J < matrix.GetLength(1); J++)
+        {
+            if(i == J) sum = sum + matrix[i, J];
+        }
+    }
+}
+SumDiagonal(matrix);
+Console.WriteLine();
+Console.WriteLine(sum);
+
